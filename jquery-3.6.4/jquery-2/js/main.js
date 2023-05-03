0e21be506033d30b0d8dfe99eb17083ee8983a15
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-04 01:02:59 +0600

    jQuery Syntax

---
+++ jquery-3.6.4/jquery-2/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // $('h2').hide(); <!-- tag ধরতে হয় quot এর মধ্যে tag দিয়ে -->
+    // $('.me').hide(); <!--class ধরতে হয় quot এর  মধ্যে ডট দিয়ে -->
+    $('#ami').hide(); <!--id ধরতে হয় quot এর  মধ্যে # দিয়ে -->
+
+});
