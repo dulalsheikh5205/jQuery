9cdd2b6a099c915028573a36208f83a49d23a842
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-13 06:15:02 +0600

    jQuery Slide part-2-.slideDown(2000)

--- jquery-3.6.4/jquery-7/js/main.js
+++ jquery-3.6.4/jquery-7/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
     $(".btn").click(function(){
-        $(".lorem").slideDown(); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideDown() এর উপর থেকে নিচে নামবে slowly  -->
+        // $(".lorem").slideDown(); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideDown() এর উপর থেকে নিচে নামবে slowly  -->
+        $(".lorem").slideDown(2000); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideDown() এর উপর থেকে নিচে নামবে slowly, speed parameter 2 second নিয়ে নামবে -->
     });
 
 });
