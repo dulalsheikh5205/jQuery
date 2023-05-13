3cd6dc4f110e26a20cd136cb0ae776b1bde374cd
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-13 08:50:37 +0600

    jQuery animate() part-1-animate({left: '200px'})

---
+++ jquery-3.6.4/jquery-8/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $("button").click(function(){
+        $("div").animate({left: '200px'});<!--  'button' কে ধরে, click করলে, div tag এর animate() left থেকে 200px সরে ডান দিকে যাবে, -->
+         
+    });
+
+});
