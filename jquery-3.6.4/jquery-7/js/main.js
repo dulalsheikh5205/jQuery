bb56e99d1385551e8d50b4b89112ffb99e2de1ef
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-13 06:07:34 +0600

    jQuery Slide part-1

---
+++ jquery-3.6.4/jquery-7/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    $(".btn").click(function(){
+        $(".lorem").slideDown(); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideDown() এর উপর থেকে নিচে নামবে slowly  -->
+    });
+
+});
