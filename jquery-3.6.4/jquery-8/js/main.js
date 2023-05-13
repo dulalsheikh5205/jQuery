51283db6d3696264a8cbec07e2517e3c785a0b96
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-13 08:55:30 +0600

    jQuery animate() part-2- .animate()

--- jquery-3.6.4/jquery-8/js/main.js
+++ jquery-3.6.4/jquery-8/js/main.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 
     $("button").click(function(){
-        $("div").animate({left: '200px'});<!--  'button' কে ধরে, click করলে, div tag এর animate() left থেকে 200px সরে ডান দিকে যাবে, -->
-         
+        // $("div").animate({left: '200px'});<!--  'button' কে ধরে, click করলে, div tag এর animate() left থেকে 200px সরে ডান দিকে যাবে, -->
+        $("div").animate({
+            left: '500px',
+            width: '200px',
+            height: '200px',
+            opacity: '0.5'
+        });  
     });
 
 });
