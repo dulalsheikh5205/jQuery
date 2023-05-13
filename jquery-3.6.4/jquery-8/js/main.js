a8ed95c54758ca337e51c30dfd4cfd56d85c74c9
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-13 09:14:01 +0600

    jQuery Animate-part-4 animate()

--- jquery-3.6.4/jquery-8/js/main.js
+++ jquery-3.6.4/jquery-8/js/main.js
@@ -2,12 +2,34 @@ $(document).ready(function(){
 
     $("button").click(function(){
         // $("div").animate({left: '200px'});<!--  'button' কে ধরে, click করলে, div tag এর animate() left থেকে 200px সরে ডান দিকে যাবে, -->
-        $("div").animate({
-            left: '500px',
-            width: '200px',
-            height: '200px',
-            opacity: '0.5'
-        });  
-    });
+        
+        /*
+         $("div").animate({
+             left: '500px',
+              width: '200px', <!-- part-2 -->
+              height: '200px',
+              opacity: '0.5'
+         }); 
+        */ 
+
+       /* part-3 
+       $("div").animate({
+        left: '500px',
+        width: '150px', <!-- part-3 -->
+        height: '150px'
+        
+    }); 
+    
+    */
+
+    /* part-4 */
+    $("div").animate({
+        left: '500px',<!-- part-4 -->
+        width: '+=150px', <!-- এখানে আগের 100px+ এখানে +150px মোট 250px width আছে, আবার ক্লিক করলে ততবার করে, বেড়ে width যুক্ত হবে, -->
+        height: '+=150px'
+        
+    }); 
+
+});
 
 });
