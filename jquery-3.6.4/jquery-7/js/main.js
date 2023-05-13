689c988061559eaa98f2210035ded81c00d165b1
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-13 06:27:26 +0600

    jQuery Slide part-3 SlideUP(200)->SlideToggle(2000)

--- jquery-3.6.4/jquery-7/js/main.js
+++ jquery-3.6.4/jquery-7/js/main.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
 
     $(".btn").click(function(){
         // $(".lorem").slideDown(); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideDown() এর উপর থেকে নিচে নামবে slowly  -->
-        $(".lorem").slideDown(2000); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideDown() এর উপর থেকে নিচে নামবে slowly, speed parameter 2 second নিয়ে নামবে -->
+        // $(".lorem").slideDown(2000); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideDown() এর উপর থেকে নিচে নামবে slowly, speed parameter 2 second নিয়ে নামবে -->
+        // $(".lorem").slideUp(); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideUp() এর নিচ থেকে উপরে উঠবে slowly -->
+        // $(".lorem").slideUp(2000); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideUp() এর নিচ থেকে উপরে উঠবে slowly, speed parameter 2 second নিয়ে উপরে উঠবে -->
+        $(".lorem").slideToggle(2000); <!--  '.btn' কে ধরে, click করলে, div '.lorem' class এর slideToggle() এর নিচ থেকে উপরে উঠবে এবং নামবে slowly, speed parameter 2 second নিয়ে উপরে উঠবে ও নামবে -->
     });
 
 });
