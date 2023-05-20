18d98b95a415d7ffb29838b8848926039afe9c05
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-20 16:39:00 +0600

    jQuery slideDown(2000)part-1

---
+++ jquery-3.6.4/jquery-9/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".moshiur").click(function(){
+        $(".lorem").slideDown(2000);  <!-- class ".moshiur" কে ধরে, click করলে, lorem class ".lorem" এর slideDown(2000) 2 second সময় নিয়ে slide নিচে নামবে, -->
+    });
+});
+
+
