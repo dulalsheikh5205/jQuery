c6fced5186cfe5a57115423ad5f854367ae4a8ab
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-20 16:53:58 +0600

    jQuery slideDown(200) and stop() part-2

--- jquery-3.6.4/jquery-9/js/main.js
+++ jquery-3.6.4/jquery-9/js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     $(".moshiur").click(function(){
         $(".lorem").slideDown(2000);  <!-- class ".moshiur" কে ধরে, click করলে, lorem class ".lorem" এর slideDown(2000) 2 second সময় নিয়ে slide নিচে নামবে, -->
     });
+
+    $("button").click(function(){
+        $(".lorem").stop();   <!-- button কে ধরে, click করলে, lorem class ".lorem" এর stop() method এর stop buttn এ click করলে lorem 'slideDown()' হওয়া stop হয়ে যাবে, -->
+    });
 });
 
 
